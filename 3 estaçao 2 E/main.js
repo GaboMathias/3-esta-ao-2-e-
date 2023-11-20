eb5a51d6cd77352cfcfe46389c09d715b5477724
Gabo Mathias Albertini Ruiz Diaz <110172162+GaboMathias@users.noreply.github.com>
2023-11-20 19:55:33 -0300

    Add files via upload

---
+++ 3 estaçao 2 E/main.js
@@ -0,0 +1,11 @@
+alert("Bem vindo a esta página, eu sou um alert, e você pode me excluir se desejar!")
+
+if(comando == "acelerar"){
+
+    alert("carro está andando");
+    
+    }else{
+    
+    alert("carro parado");
+    
+    } 
